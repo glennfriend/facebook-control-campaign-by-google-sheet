8f34ab76a30a7c692ba71f9393c34726d0f87501
root <root@training3.training3>
2016-10-25 20:44:10 -0700

    add example.config.php

--- example.config.php
+++ example.config.php
@@ -3,5 +3,20 @@
  *  這裡的設定值, 將會覆蓋 core/config/ 下相同名稱的值
  */
 return [
-
+    'app' => [
+        // 不論在任何情況下, 正式的環境都必須使用 'production' 為 env 的值
+        // 測試環境請使用 'training'
+        'env' => 'production',
+    ],
+    'google' => [
+        'sheet'  => [
+            'file_id' => 'xxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        ]
+    ],
+    'facebook' => [
+        'app' => [
+            'id'     => 'xxxxxxxxxxxxxxx',
+            'secret' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        ],
+    ],
 ];
