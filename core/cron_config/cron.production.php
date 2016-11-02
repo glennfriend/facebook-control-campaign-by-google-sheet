526ec3847953569a5970327bc08f5e11840bab38
root <root@training3.training3>
2016-11-02 01:45:35 -0700

    fix php path problem

--- core/cron_config/cron.production.php
+++ core/cron_config/cron.production.php
@@ -8,6 +8,6 @@ return [
      *  每天早上
      *      - 依照 google sheet 的設定, 改變 facebook 的 狀態
      */
-    ['10 0 * * *', "php {$shellPath}/execute-facebook-active-pause-by-google-sheet.php > /dev/null 2>&1"],
+    ['10 0 * * *', "/root/.phpbrew/php/php-7.0.10/bin/php {$shellPath}/execute-facebook-active-pause-by-google-sheet.php > /dev/null 2>&1"],
 
 ];
